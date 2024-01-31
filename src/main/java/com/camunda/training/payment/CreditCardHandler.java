0290d1ffef510d052c9f941abbd03251d179e564
Hendrik Kupitz <hendrik.kupitz@camunda.com>
2024-01-31 14:51:07 +0100

    Add solution for exercise 8

--- src/main/java/com/camunda/training/payment/CreditCardHandler.java
+++ src/main/java/com/camunda/training/payment/CreditCardHandler.java
@@ -1,16 +1,23 @@
 package com.camunda.training.payment;
 
+import com.camunda.training.services.CreditCardService;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import org.camunda.bpm.client.spring.annotation.ExternalTaskSubscription;
 import org.camunda.bpm.client.task.ExternalTask;
 import org.camunda.bpm.client.task.ExternalTaskHandler;
 import org.camunda.bpm.client.task.ExternalTaskService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 @ExternalTaskSubscription("creditCardCharging")
 public class CreditCardHandler implements ExternalTaskHandler {
+  
+  @Autowired
+  private CreditCardService service;
 
   private static final Logger LOG = LoggerFactory.getLogger(CustomerCreditHandler.class);
 
@@ -18,6 +25,18 @@ public class CreditCardHandler implements ExternalTaskHandler {
   public void execute(ExternalTask externalTask, ExternalTaskService externalTaskService) {
     LOG.info("handle topic {} for task id {}", externalTask.getTopicName(), externalTask.getId());
 
-    externalTaskService.complete(externalTask);
+    String cvc = externalTask.getVariable("CVC");
+    String cardNumber = externalTask.getVariable("cardNumber");
+    String expiryDate = externalTask.getVariable("expiryDate");
+    Double openAmount = externalTask.getVariable("openAmount");
+
+    try {
+      service.chargeAmount(cardNumber, cvc, expiryDate, openAmount);
+      externalTaskService.complete(externalTask);
+    } catch (IllegalArgumentException e) {
+      StringWriter sw = new StringWriter();
+      e.printStackTrace(new PrintWriter(sw));
+      externalTaskService.handleFailure(externalTask, "credit card expired", sw.toString(), 0, 0);
+    }
   }
 }
